4f02c2328acf080b14a67fabb31068807c80b9e5
Felipe Santos <fezto.17@gmail.com>
2021-11-04 17:18:07 -0300

    Initial Commit

---
+++ Chuck-Jokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chuck-Jokes
+//
+//  Created by Felipe Santos on 04/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
